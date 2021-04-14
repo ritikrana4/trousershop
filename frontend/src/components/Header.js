c4df1f2e6128ae79f13c40efa13f8fed446767b4
ritik <ritikrana4@gmail.com>
2021-04-14 18:18:21 +0530

    minor fixes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -25,8 +25,10 @@ const Header = () => {
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Route render={({ history }) => <SearchBox history={history} />} />
             <Nav className="ml-auto">
+              <Route
+                render={({ history }) => <SearchBox history={history} />}
+              />
               <LinkContainer to="/cart">
                 <Nav.Link>
                   <i className="fas fa-shopping-cart"></i> Cart
